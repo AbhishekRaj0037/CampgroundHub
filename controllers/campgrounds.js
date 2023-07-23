947cf40a00f9379c5b4dbedb205ae19b17b6e322
Abhishek Raj <me_abhishek.tethered@icloud.com>
2023-07-24 00:41:44 +0530

    Removed unwanted package from package.json

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,8 +1,5 @@
 const Campground = require("../models/campground");
 const { cloudinary } = require("../cloudinary");
-const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
-const mapBoxToken = process.env.MAPBOX_TOKEN;
-const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 
 module.exports.index = async (req, res) => {
   const campgrounds = await Campground.find({});
